e4f14f8243afda4b279d0416fe929fee8d23a703
Farce <intbusfor@gmail.com>
2019-09-02 04:47:52 +0100

    Pushes calculator app to repository

---
+++ script.js
@@ -0,0 +1,89 @@
+function getHistory(){
+	return document.getElementById("express").innerText;
+}
+function printHistory(num){
+	document.getElementById("express").innerText=num;
+}
+function getOutput(){
+	return document.getElementById("res").innerText;
+}
+function printOutput(num){
+	if(num==""){
+		document.getElementById("res").innerText=num;
+	}
+	else{
+		document.getElementById("res").innerText=getFormattedNumber(num);
+	}	
+}
+function getFormattedNumber(num){
+	if(num=="-"){
+		return "";
+	}
+	var n = Number(num);
+	var value = n.toLocaleString("en");
+	return value;
+}
+function reverseNumberFormat(num){
+	return Number(num.replace(/,/g,''));
+}
+var operator = document.getElementsByClassName("operator");
+for(var i =0;i<operator.length;i++){
+	operator[i].addEventListener('click',function(){
+		if(this.id=="clear"){
+			printHistory("");
+			printOutput("");
+		}
+		else if(this.id=="backspace"){
+			var output=reverseNumberFormat(getOutput()).toString();
+			if(output){//if output has a value
+				output= output.substr(0,output.length-1);
+				printOutput(output);
+			}
+		}
+		else{
+			var output=getOutput();
+			console.log('output', output);
+			var history=getHistory();
+			if(output==""&&history!=""){
+				if(isNaN(history[history.length-1])){
+					history= history.substr(0,history.length-1);
+				}
+			}
+			if(output!="" || history!=""){
+				output= output==""?output:reverseNumberFormat(output);
+				history=history+output;
+				if(this.id=="="){
+					var result=eval(history);
+					printOutput(result);
+					printHistory("");
+				}
+				else{
+					history=history+this.id;
+					printHistory(history);
+					printOutput("");
+				}
+			}
+		}
+		
+	});
+}
+var number = document.getElementsByClassName("number");
+for(var i =0;i<number.length;i++){
+	number[i].addEventListener('click',function(){
+		var output=reverseNumberFormat(getOutput());
+		if(output!=NaN){ //if output is a number
+			output=output+this.id;
+			printOutput(output);
+		}
+	});
+}
+
+function evaluate(input){
+	try{
+		var result = eval(input);
+		document.getElementById("res").innerText = result;
+	}
+	catch(e){
+		document.getElementById("res").innerText = "";
+	}
+}
